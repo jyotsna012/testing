5018b235a90254b1f47dc72b8d2cd89e9542d6df
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-07 15:27:17 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+
+   
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
 int size;   
 cin >> size;
 int *A = new int[size];//dynamically assign memory to pointer A 
