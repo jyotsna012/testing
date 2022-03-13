edc2fcc26c73bdba4810833394a64ca1d35583aa
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-12 17:12:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
   int x;
   cout << "Type a number: "; // Type a number and press enter
   cin >> x; // Get user input from the keyboard
-  cout << "Your number is: " << x;
+  cout << "Your number is: " << x << endl;
   int *A = new int[x];
   A[0] = 1;
   A[1] = 2;
