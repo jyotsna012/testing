2ada819eedba311029beb00646f03f766e6f5d66
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-03-12 18:04:11 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,15 +7,17 @@ int main() {
   cin >> x; // Get user input from the keyboard
   cout << "Your number is: " << x << endl;
   int *A = new int[x];
-  A[0] = 1;
+  /*A[0] = 1;
   A[1] = 2;
   A[2] = 3;
   A[3] = 4;
   A[4] = 5;
-  cout << "this is A1: " << A[1] << endl;
+  cout << "this is A1: " << A[1] << endl;*/
   
-  int len = sizeof(A)/sizeof(A[0]);
-  cout << "The length of the array is: " << len;
+  for (int i = 0; i < x; i++) {
+  A[i] = i;
+  cout << A[i] << "\n";
+  }
   
   delete[] A;
   
